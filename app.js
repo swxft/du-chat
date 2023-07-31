a717620456b1589f8e744c202e0d7ea5eb746a43
swxft <edenweb@outlook.com>
2023-07-31 15:05:09 -0400

    listen for new user + 🧹 code

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-//App.js
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
-// const io = require('socket.io')(server);
-// io.on("connection", (socket) => {
-//   console.log("🔌 New user connected! 🔌");
-// })
 const io = require('socket.io')(server);
 io.on("connection", (socket) => {
-  // This file will be read on new socket connections
   require('./sockets/chat.js')(io, socket);
 })
 
-//Express View Engine for Handlebars
 const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'index' }));
 app.set('view engine', 'handlebars');
--- public/index.js
+++ public/index.js
@@ -1,15 +1,21 @@
 // index.js
-$(document).ready(()=>{
+$(document).ready(() => {
+    
     const socket = io.connect();
 
-    $('#create-user-btn').click((e)=>{
-      e.preventDefault();
-      let username = $('#username-input').val();
-      if(username.length > 0){
-        //Emit to the server the new user
-        socket.emit('new user', username);
-        $('.username-form').remove();
-      }
+    $('#create-user-btn').click((e) => {
+        e.preventDefault();
+        let username = $('#username-input').val();
+        if(username.length > 0){
+            //Emit to the server the new user
+            socket.emit('new user', username);
+            $('.username-form').remove();
+        }
     });
 
-  })
+    //socket listeners
+    socket.on('new user', (username) => {
+        console.log(`✋ ${username} has joined the chat! ✋`);
+    })
+
+})
