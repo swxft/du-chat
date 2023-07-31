0f69f842ecfca5ae0da3f3768ac073139d7edc53
swxft <edenweb@outlook.com>
2023-07-31 14:47:59 -0400

    checkpoint

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 //App.js
 const express = require('express');
 const app = express();
-//Socket.io has to use the http server
 const server = require('http').Server(app);
+
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected! 🔌");
+})
 
 //Express View Engine for Handlebars
 const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+//Establish your public folder
+app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index.handlebars');
-})
+    res.render('index.handlebars');
+  })
 
 server.listen('3000', () => {
   console.log('Server listening on Port 3000');
