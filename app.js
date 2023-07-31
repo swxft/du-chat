3e62c7ae347e1dc4426d6808fc21f09be619fcc0
swxft <edenweb@outlook.com>
2023-07-31 15:18:41 -0400

    Users can create new channels and join

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express();
 const server = require('http').Server(app);
 
 const io = require('socket.io')(server);
-//We'll store our online users here
 let onlineUsers = {};
+//Save the channels in this object.
+let channels = {"General" : []};
+
 io.on("connection", (socket) => {
-  // Make sure to send the users to our chat file
-  require('./sockets/chat.js')(io, socket, onlineUsers);
-})
+  // Make sure to send the channels to our chat file
+  require('./sockets/chat.js')(io, socket, onlineUsers, channels);
+});
 
 const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'index' }));
